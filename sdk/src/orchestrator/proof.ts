de19756a858527d66803b3fb89cdba78d782235c
samuel1-ona <samuonanikesamuel123@gmail.com>
2026-02-03 13:02:14 +0100

    Implemented progressive event tracking

--- sdk/src/orchestrator/proof.ts
+++ sdk/src/orchestrator/proof.ts
@@ -1,6 +1,8 @@
 import { CompiledCircuit } from '@noir-lang/types';
+import { EventEmitter } from 'events';
 import { NoahProver, NoahProverInputs } from '../circuit/prover';
 import { NoahContractManager, NoahConfig } from '../contract/manager';
+import { NoahError, NoahProverError, NoahContractError } from '../utils/errors';
 
 export interface OrchestratorConfig {
     circuitArtifact: CompiledCircuit;
@@ -8,11 +10,20 @@ export interface OrchestratorConfig {
     starknet: NoahConfig;
 }
 
-export class NoahProofOrchestrator {
+export enum NoahEvent {
+    PROOF_GENERATION_START = 'proof_generation_start',
+    PROOF_GENERATION_SUCCESS = 'proof_generation_success',
+    TRANSACTION_SUBMISSION_START = 'transaction_submission_start',
+    TRANSACTION_SUBMISSION_SUCCESS = 'transaction_submission_success',
+    ERROR = 'error'
+}
+
+export class NoahProofOrchestrator extends EventEmitter {
     private prover: NoahProver;
     private contracts: NoahContractManager;
 
     constructor(config: OrchestratorConfig) {
+        super();
         this.prover = new NoahProver(config.circuitArtifact, config.vk);
         this.contracts = new NoahContractManager(config.starknet);
     }
@@ -23,22 +34,34 @@ export class NoahProofOrchestrator {
      * @returns The transaction result
      */
     async proveAndVerify(inputs: NoahProverInputs) {
-        console.log('Generating proof...');
-        const proof = await this.prover.generateProof(inputs);
+        try {
+            this.emit(NoahEvent.PROOF_GENERATION_START);
+            console.log('Generating proof...');
+            const proof = await this.prover.generateProof(inputs);
+            this.emit(NoahEvent.PROOF_GENERATION_SUCCESS, proof);
 
-        console.log('Preparing Starknet calldata...');
-        const calldata = await this.prover.getStarknetCalldata(proof);
+            this.emit(NoahEvent.TRANSACTION_SUBMISSION_START);
+            console.log('Preparing Starknet calldata...');
+            const calldata = await this.prover.getStarknetCalldata(proof);
 
-        console.log('Submitting to Starknet...');
-        const tx = await this.contracts.registry.verifyCredential(
-            calldata,
-            inputs.current_year,
-            inputs.current_month,
-            inputs.current_day,
-            inputs.min_age
-        );
+            console.log('Submitting to Starknet...');
+            const tx = await this.contracts.registry.verifyCredential(
+                calldata,
+                inputs.current_year,
+                inputs.current_month,
+                inputs.current_day,
+                inputs.min_age
+            );
+            this.emit(NoahEvent.TRANSACTION_SUBMISSION_SUCCESS, tx);
 
-        return tx;
+            return tx;
+        } catch (error: any) {
+            const noahError = error instanceof NoahError
+                ? error
+                : new NoahProverError(error.message || 'Unknown orchestrator error');
+            this.emit(NoahEvent.ERROR, noahError);
+            throw noahError;
+        }
     }
 
     async destroy() {
