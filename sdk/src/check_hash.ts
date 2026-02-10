5608b7e801f8798a8522753b87a270817ff035e9
samuel1-ona <samuonanikesamuel123@gmail.com>
2026-02-10 09:21:14 +0100

    Deleted debug test files

--- sdk/src/check_hash.ts
+++
@@ -1,7 +0,0 @@
-
-const vk_hash_dec = "18537972937098229147612897403925779204278329535054842827205954724082797308089";
-const vk_hash_hex = BigInt(vk_hash_dec).toString(16);
-console.log(`VK Hash Hex: 0x${vk_hash_hex}`);
-
-const target = "000000000000000000000000000000000029ffcc382cfcfbed7748bb5a391a3e";
-console.log(`Target:  0x${target}`);
